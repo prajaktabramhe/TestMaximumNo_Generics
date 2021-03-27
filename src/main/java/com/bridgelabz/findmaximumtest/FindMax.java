e2208561088aeea80ea46e7c6fa42423b9431841
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-27 23:57:05 +0530

    [Prajakta]: used compareTo method to find the maximum Float number and passed the parameter using test case

--- src/main/java/com/bridgelabz/findmaximumtest/FindMax.java
+++ src/main/java/com/bridgelabz/findmaximumtest/FindMax.java
@@ -1,12 +1,27 @@
 package com.bridgelabz.findmaximumtest;
 
 public class FindMax {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("***** Welcome to arithmetic number computation ****");
     }
 
     public Integer findMaxValue(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
         Integer max = firstNumber;
+        if (secondNumber.compareTo(max) > 0)
+        {
+            max = secondNumber;
+        }
+        if (thirdNumber.compareTo(max) > 0)
+        {
+            max = thirdNumber;
+        }
+        printMax(max);
+        return max;
+    }
+    public Float findMaxValue(Float firstNumber, Float secondNumber, Float thirdNumber)
+    {
+        Float max = firstNumber;
         if (secondNumber.compareTo(max) > 0) {
             max = secondNumber;
         }
@@ -16,8 +31,12 @@ public class FindMax {
         printMax(max);
         return max;
     }
-
-    void printMax(Integer max) {
+    void printMax(Integer max)
+    {
         System.out.println("Maximum number is  = " + max);
+    }
+    void printMax(Float max)
+    {
+        System.out.println("Maximum from float = " + max);
     }
 }
--- src/test/java/com/bridgelabz/findmaximumtest/FindMaxTest.java
+++ src/test/java/com/bridgelabz/findmaximumtest/FindMaxTest.java
@@ -9,7 +9,7 @@ public class FindMaxTest
 {
     FindMax findMax = new FindMax();
 
-
+ //for Integer Number
     @Test
     public void givenFirstNumberAsLarge_ShouldReturn_FirstNumberAsMax() {
         Integer max = findMax.findMaxValue(300, 25, 105);
@@ -26,6 +26,25 @@ public class FindMaxTest
     public void givenThirdNumberAsMaximum_ShouldReturnThirdNumberAsMaximum() {
         Integer max = findMax.findMaxValue(10,20,100);
         Assertions.assertEquals((Integer) 100,max);
+    }
+
+    //for Float Number
+    @Test
+    public void givenFirstFloatAsMaximum_ShouldReturnFirstFloatAsMaximum() {
+        Float max1 = findMax.findMaxValue(20f, 15f, 10f);
+        Assertions.assertEquals((Float) 20f, max1);
+    }
+
+    @Test
+    public void givenSecondFloatAsMaximum_ShouldReturnSecondFloatAsMaximum() {
+        Float max1 = findMax.findMaxValue(22f, 30f, 10f);
+        Assertions.assertEquals((Float) 30f, max1);
+    }
+
+    @Test
+    public void givenThirdFloatAsMaximum_ShouldReturnThirdFloatAsMaximum() {
+        Float max1 = findMax.findMaxValue(20f, 30f, 40f);
+        Assertions.assertEquals((Float) 40f, max1);
     }
 }
 
